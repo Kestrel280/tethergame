2f5343a74a78fc053a02536781cbab3e95a51b4f
Kestrel280 <downey.samp@gmail.com>
2025-07-22 15:56:47 -0400

    Add pre-move collision check to enable surfing

--- controllers/movement/movement_controller_player.gd
+++ controllers/movement/movement_controller_player.gd
@@ -12,5 +12,10 @@ func move(dt : float, wish_dir : Vector3):
 	var new_state_name : StringName = movement_state_machine.current_state.try_move(dt, wish_dir, jumping);
 	if new_state_name != movement_state_machine.current_state.state_name:
 		movement_state_machine.transition(new_state_name);
-
+	
+	# Test movement without actually performing it
+	# If we collided, do our own slide calculation first, before doing move_and_slide
+	var collision = body.move_and_collide(body.velocity * dt, true);
+	if collision: body.velocity = body.velocity.slide(collision.get_normal());
+	
 	body.move_and_slide();
