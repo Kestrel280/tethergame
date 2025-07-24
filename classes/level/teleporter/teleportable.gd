cd04bd6cab7f82f91df7f2203bc17cfccb7a1c99
Kestrel280 <downey.samp@gmail.com>
2025-07-23 20:22:37 -0400

    Remove redundant Teleportable scene, clarify Teleportable comments

--- classes/level/teleporter/teleportable.gd
+++ classes/level/teleporter/teleportable.gd
@@ -1,9 +1,6 @@
 class_name Teleportable
 extends Node
 
-# Attach to a collision shape. If the collision shape enters a teleporter,
-# 	the teleporter will check the collision shape owner's children for the
-# 	existence of a Teleportable node.
-
-@export var body : Node3D; # Body to be teleported (position, velocity, & x-rotation)
-@export var head : Node3D; # If teleporter has set_view_angle, 
+# Attach to a node with a collision shape. If the collision shape enters a
+# 	teleporter, the teleporter will check the collision shape owner's children
+# 	for the existence of a Teleportable node.
