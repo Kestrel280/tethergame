95ba906435f80f3e15e63b1d67d940dd1c07ba22
Kestrel280 <downey.samp@gmail.com>
2025-08-02 20:26:52 -0400

    Increase gravity, increase player air accel

--- classes/player/player_settings.gd
+++ classes/player/player_settings.gd
@@ -3,9 +3,9 @@ extends Node
 
 
 static var air_speed_cap : float = 0.5;
-static var air_accel : float = 150.0;
+static var air_accel : float = 1000.0;
 static var max_ground_speed : float  = 5.0;
 static var ground_accel : float  = 0.15;
 static var ground_friction : float  = 0.3;
-static var jump_impulse : float  = 4.0;
+static var jump_impulse : float  = 8.0;
 static var kbm_sensitivity : float = 1.0;
