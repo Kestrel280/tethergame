2119b92f2553459deb6e54867af4f3d802853bb3
Kestrel280 <downey.samp@gmail.com>
2025-08-03 00:18:13 -0400

    Further tweak player movement params, switch to cylinder collision shape for player

--- classes/player/player_settings.gd
+++ classes/player/player_settings.gd
@@ -2,10 +2,10 @@ class_name Player_Settings
 extends Node
 
 
-static var air_speed_cap : float = 0.5;
-static var air_accel : float = 1000.0;
-static var max_ground_speed : float  = 5.0;
-static var ground_accel : float  = 0.15;
+static var air_speed_cap : float = 0.76;
+static var air_accel : float = 400.0;
+static var max_ground_speed : float  = 6.6;
+static var ground_accel : float  = 0.12;
 static var ground_friction : float  = 0.3;
-static var jump_impulse : float  = 8.0;
+static var jump_impulse : float  = 7.52;
 static var kbm_sensitivity : float = 1.0;
